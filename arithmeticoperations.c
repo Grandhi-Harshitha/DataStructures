4b14195880620baf4b66f8e3100ddf4062cda059
Grandhi-Harshitha <120390344+Grandhi-Harshitha@users.noreply.github.com>
2022-12-12 20:46:55 +0530

    Add files via upload

---
+++ arithmeticoperations.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum,substraction,multiplication,division;
+printf("enter a number a&b");
+scanf("%d%d",&a,&b);
+sum=(a+b);
+printf("sum of two numbers is %d\n",sum);
+substraction=(a-b);
+printf("substraction of two numbers is %d\n",substraction);
+multiplication=(a*b);
+printf("multiplication of two numbers is %d\n",multiplication);
+division=(a/b);
+printf("division of two numbers is %d\n",division);
+return;
+}
+
+
